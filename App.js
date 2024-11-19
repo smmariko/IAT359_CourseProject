2d966c6875fb4ddfb31f9f9e2a4090f98a860acf
jonethenz <supangat_jonathan@yahoo.com>
2024-11-18 20:17:13 -0800

    updated app.js to be able to navigate all the screen

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import 'react-native-get-random-values';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { LoginScreen, SignUpScreen } from './LoginScreen';
+import HomeScreen from './HomeScreen';
+import AddRestaurantScreen from './AddRestaurantScreen';
+import RestaurantListScreen from './RestaurantListScreen';
+import EditRestaurantScreen from './EditRestaurantScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +16,10 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddRestaurant" component={AddRestaurantScreen} />
+        <Stack.Screen name="RestaurantList" component={RestaurantListScreen} />
+        <Stack.Screen name="EditRestaurant" component={EditRestaurantScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
